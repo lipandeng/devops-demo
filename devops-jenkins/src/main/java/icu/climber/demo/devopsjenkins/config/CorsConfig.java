666228cabf13118c2d7f313fffffe45801c07025
lipandeng <li.pandeng@163.com>
2021-09-08 16:30:22 +0800

    支持跨域请求

---
+++ devops-jenkins/src/main/java/icu/climber/demo/devopsjenkins/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package icu.climber.demo.devopsjenkins.config;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Description 跨域
+ * @Created by lipandeng on 2021/9/8
+ */
+@SpringBootConfiguration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //添加映射路径
+        registry.addMapping("/**")
+                //是否发送Cookie
+                .allowCredentials(true)
+                //设置放行哪些原始域   SpringBoot2.4.4下低版本使用.allowedOrigins("*")
+                .allowedOriginPatterns("*")
+                //放行哪些请求方式
+                .allowedMethods(new String[]{"GET", "POST", "PUT", "DELETE"})
+                //.allowedMethods("*") //或者放行全部
+                //放行哪些原始请求头部信息
+                .allowedHeaders("*")
+                //暴露哪些原始请求头部信息
+                .exposedHeaders("*");
+    }
+}
